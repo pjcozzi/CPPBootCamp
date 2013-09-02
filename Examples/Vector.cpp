47c8e94d1d025ffaecc835ac1249950f3234c890
Patrick Cozzi <pjcozzi@gmail.com>
2013-09-02 16:08:59 -0400

    Update vector example

--- Examples/Vector.cpp
+++ Examples/Vector.cpp
@@ -1,19 +1,28 @@
 #include "stdafx.h"
 
-// encapsulation - public vs. private
-// static
-// naming conventions
-// const correct
-// operator=
-// inline
-// namespace
-// function by value
-// header
-// template
+// Notes
+// -----
 
-// inheritance
-// virtual function
-// abstract classes
+// [ ] encapsulation - public vs. private
+// [ ] static
+// [ ] naming conventions: pascal case, camel case, ...
+// [ ] const correct
+// [ ] copy constructor
+// [ ] operator=
+// [ ] inline
+// [ ] namespace
+// [ ] pass by value vs. pass by reference
+// [ ] headers and cpp files
+// [ ] template
+// [ ] function overloading
+// [ ] global vs. local variables
+// [ ] stack vs. heap.  & vs. new
+// [ ] 2D arrays
+// [ ] enums
+
+// [ ] inheritance
+// [ ] virtual function
+// [ ] interface vs. abstract classes
 
 class Vector
 {
@@ -24,6 +33,13 @@ public:
           z(0.0f)
     {
     }
+
+	Vector(const Vector& v)
+        : x(v.x),
+          y(v.y),
+          z(v.z)
+	{
+	}
 
     Vector(float x, float y, float z)
         : x(x),
@@ -36,6 +52,17 @@ public:
     {
     }
 
+	Vector& operator=(const Vector& other)
+	{
+		if (this != &other)
+		{
+			x = other.x;
+			y = other.y;
+			z = other.z;
+		}
+		return *this;
+	}
+
     void add(const Vector& other)
     {
         x += other.x;
@@ -43,15 +70,27 @@ public:
         z += other.z;
     }
 
+	Vector& operator+=(const Vector& other)
+	{
+		add(other);
+		return *this;
+	}
+
     float x;
     float y;
     float z;
 };
 
+bool operator==(const Vector& left, const Vector& right)
+{
+	return (left.x == right.x) && (left.y == right.y) && (left.z == right.z);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
 int _tmain(int argc, _TCHAR* argv[])
 {
     Vector v = Vector(1.0f, 2.0f, 3.0f);
-    v.add(v);
 
     return 0;
 }
